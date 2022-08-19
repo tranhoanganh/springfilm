8bc5a8c922ef22f9b81fa2f7d33cc7683ad424fa
Anh Tran <anhth.na@gmail.com>
2022-08-19 17:51:45 +0700

    Update url domain

--- src/main/java/com/demo/film/config/Constant.java
+++ src/main/java/com/demo/film/config/Constant.java
@@ -1,7 +1,7 @@
 package com.demo.film.config;
 
 public class Constant {
-    public static String SERVER = "http://10.0.2.2:8080/";
+    public static String SERVER = "https://springfilm.herokuapp.com/";
     public static String LINK_IMAGES = SERVER + "images/";
     public static String LINK_MOVIES_IMAGE = LINK_IMAGES + "movies/";
     public static String LINK_VIDEOS = SERVER + "videos/";
