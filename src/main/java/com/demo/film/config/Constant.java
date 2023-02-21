5c0908faa9bd8d7bf1ce1bd15764388229be51cd
tranhoanganh <anhth.na@gmail.com>
2023-02-22 01:50:52 +0700

    Update Constant.java

--- src/main/java/com/demo/film/config/Constant.java
+++ src/main/java/com/demo/film/config/Constant.java
@@ -2,7 +2,7 @@ package com.demo.film.config;
 
 public class Constant {
     public static String SERVER = "https://springfilm.herokuapp.com/";
-    public static String LINK_IMAGES = SERVER + "images/";
+    public static String LINK_IMAGES = "images/";
     public static String LINK_MOVIES_IMAGE = LINK_IMAGES + "movies/";
-    public static String LINK_VIDEOS = SERVER + "videos/";
+    public static String LINK_VIDEOS = "videos/";
 }
